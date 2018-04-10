ca220d6cc917efc06e26d94fe1838c1b24516bcf
aradzekler <25280958+aradzekler@users.noreply.github.com>
2018-04-10 20:15:36 +0300

    Create Member.h

---
+++ Member.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef MEMBER
+#define MEMBER
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Member {
+private:
+	string name;
+    static int memberCount;
+    map<string, Member> *followingList;
+    map<string, Member> *followersList;
+	bool created;
+public:
+    Member();
+    ~Member();
+    int numFollowers();
+    int numFollowing();
+    void follow(Member &member);
+    void unfollow(Member &member);
+	static int count();
+};
+
+#endif 
