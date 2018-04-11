8cf2756fb2b2cd5504b26abc80a3e3f61063a280
aradzekler <25280958+aradzekler@users.noreply.github.com>
2018-04-12 00:22:45 +0300

    Update Member.cpp

--- Member.cpp
+++ Member.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <list>
 #include "Member.h"
 using namespace std;
 
@@ -13,59 +11,49 @@ int Member::count() {
 }
 
 Member::Member() {
-    id = ++memberCount;
+    id = idCounter;
     idCounter++;
-    onlineUsers.push_back(memberCount);
     memberCount++; // every obj creation adds to counter.
 }
 
 Member::~Member(){
-    onlineUsers.remove(id);
     memberCount--;
 }
 
-void Member::follow(Member& member) {
-       if (find(followingList.begin(),followingList.end(), member.id) == followingList.end()) {
-            followingList.push_back(member.id);
-            member.followersList.push_back(id);
-     }
+void Member::follow(Member &member) {
+    if (member.id == id) { // checks if following self.
+        return;
+    }
+    for(int i = 0; i < followingList.size(); i++) {
+       if ((followingList[i]->id) == (member.id)) { // if already exists.
+        return;
+         }
+    }
+    followingList.push_back(&member);      // add to lists.
+    member.followersList.push_back(this);
 }
-
 // unfollows (takes care of both lists)
-void Member::unfollow(Member& member) {
-    for (int i = 0; i<followingList.size(); i++) {
-        if(followingList[i] == member.id) {
-            followingList.erase(followingList.begin() +i); 
+void Member::unfollow(Member &member) {
+    for (int i = 0; i < followingList.size(); i++) {
+        if(followingList[i] == &member) {
+            followingList.erase(followingList.begin()+i);
+            break;
         }
     }
-    for (int i = 0; i<member.followersList.size(); i++) {
-        if (member.followersList[i] == id) {
-            member.followersList.erase(member.followersList.begin() +i);
+    for(int i = 0; i<followersList.size(); i++) {
+        if(followersList[i] == &member) {
+            followersList.erase(followersList.begin()+i);
+            break;
         }
     }
 }
 
 // return number of members this one is following.
 int Member::numFollowing() {
-       for (int i=0;i<followingList.size();i++) {
-        list<int>::iterator it;
-        it= find(onlineUsers.begin(), onlineUsers.end(), followingList[i]);
-        if (it == onlineUsers.end()){
-            followingList.erase(followingList.begin()+i);
-        }
-    }
     return followingList.size();
 }
 
 // returns number of followers.
 int Member::numFollowers() {
-    for(int i=0;i<followersList.size();i++) {
-        list<int>::iterator it;
-        it= find(onlineUsers.begin(), onlineUsers.end(), followersList[i]);
-        if(it == onlineUsers.end()) {
-            followersList.erase(followersList.begin()+i);
-        }
-    }
     return followersList.size();
 }
-
