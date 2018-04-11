15e5d486e8aa15c11f549022fe24bb9501795844
aradzekler <25280958+aradzekler@users.noreply.github.com>
2018-04-12 00:08:19 +0300

    Update Member.h

--- Member.h
+++ Member.h
@@ -3,23 +3,25 @@
 #define MEMBER
 #include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
 
 class Member {
 private:
-	string name;
-	int id; // unique object id.
+    string name;
+    int id; // unique object id.
     vector<Member*> followingList; // vectors hold following and followers.
     vector<Member*> followersList;
 public:
-	static int memberCount; // static for counting total objects.
-	static int idCounter; 
-	static int count();
+    static int memberCount; // static for counting total objects.
+    static int idCounter; 
+    static int count();
+    static list<int> onlineUsers;
     Member();
     ~Member();
     void follow(Member &member);
     void unfollow(Member &member);
-	int numFollowing();
+    int numFollowing();
     int numFollowers();
 };
 
